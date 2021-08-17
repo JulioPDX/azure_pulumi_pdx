1963ff2752e9e5ef9b240bed0ec3bb8aac7b91d9
juliopdx <juliop503@gmail.com>
2021-08-17 16:47:11 -0700

    pushing latest

--- __main__.py
+++ __main__.py
@@ -1,11 +1,18 @@
 """An Azure RM Python Pulumi program"""
 
+import os
 import pulumi
-from pulumi_azure_native import resources, network
-from pulumi_azure_native.resources import resource
+from pulumi_azure_native import resources, network, compute
+from infra.vms import VM_DATA
+
+rg_name = "juliopdx_rg_dev"
+# from pulumi_azure_native.resources import resource
 
 # Create an Azure Resource Group
-resource_group = resources.ResourceGroup("juliopdx_rg_dev")
+# resource_group = resources.ResourceGroup("juliopdx_rg_dev")
+resource_group = resources.ResourceGroup("juliopdx_rg_dev",resource_group_name=rg_name)
+
+pulumi.export("resource_group_nm", resource_group.name)
 
 VNETS = {
     "CoreServicesVnet": {
@@ -48,11 +55,104 @@ for vnet, values in VNETS.items():
         resource_group_name=resource_group.name,
         location=values["region"],
     )
+# for vnet, values in VNETS.items():
     for subnet in values["subnets"]:
         sub = network.Subnet(
             subnet["name"],
             address_prefix=subnet["subnet"],
             resource_group_name=resource_group.name,
             subnet_name=subnet["name"],
-            virtual_network_name=vnet,
+            virtual_network_name=net.name,
         )
+
+# Private DNS Zone
+private_zone = network.PrivateZone(
+    "privateZone",
+    location="Global",
+    private_zone_name="azure_pulumi.com",
+    resource_group_name=resource_group.name,
+)
+
+
+# Virtual Network Links to Private DNS
+# Having a serious issue using resource_group.name here
+# ... very odd
+for vnet, values in VNETS.items():
+    virtual_network_link = network.VirtualNetworkLink(
+        vnet,
+        location="Global",
+        private_zone_name="azure_pulumi.com",
+        registration_enabled=True,
+        resource_group_name=resource_group.name,
+        virtual_network=network.SubResourceArgs(
+            id=f"/subscriptions/{os.environ['SUB_ID']}/resourceGroups/{rg_name}/providers/Microsoft.Network/virtualNetworks/{vnet}"
+        ),
+        virtual_network_link_name=f"{vnet}-NetworkLink",
+    )
+
+for k, v in VM_DATA.items():
+    public_ip_address = network.PublicIPAddress(
+        f"{k}-publicIPAddress",
+        location=v["location"],
+        public_ip_address_name=f"{k}-pip",
+        resource_group_name=rg_name,
+    )
+
+    network_interface = network.NetworkInterface(
+        f"{k}-networkInterface",
+        enable_accelerated_networking=True,
+        ip_configurations=[
+            network.NetworkInterfaceIPConfigurationArgs(
+                name="ipconfig1",
+                public_ip_address=network.PublicIPAddressArgs(
+                    id=f"/subscriptions/{os.environ['SUB_ID']}/resourceGroups/{rg_name}/providers/Microsoft.Network/publicIPAddresses/{k}-pip",
+                ),
+                subnet=network.SubnetArgs(
+                    id=f"/subscriptions/{os.environ['SUB_ID']}/resourceGroups/{rg_name}/providers/Microsoft.Network/virtualNetworks/{v['nic_vnet']}/subnets/{v['nic_subnet']}",
+                ),
+            )
+        ],
+        location=v["location"],
+        network_interface_name=v["nic_name"],
+        resource_group_name=rg_name,
+    )
+
+for k, v in VM_DATA.items():
+    virtual_machine = compute.VirtualMachine(
+        f"{v} virtualMachine build",
+        hardware_profile=compute.HardwareProfileArgs(
+            vm_size="Standard_D1_v2",
+        ),
+        location=v["location"],
+        network_profile=compute.NetworkProfileArgs(
+            network_interfaces=[
+                compute.NetworkInterfaceReferenceArgs(
+                    id=f"/subscriptions/{os.environ['SUB_ID']}/resourceGroups/{rg_name}/providers/Microsoft.Network/networkInterfaces/{v['nic_name']}",
+                    primary=True,
+                )
+            ],
+        ),
+        os_profile=compute.OSProfileArgs(
+            admin_password="JulioPDX789!@#",
+            admin_username="juliopdx",
+            computer_name=k,
+        ),
+        resource_group_name=rg_name,
+        storage_profile=compute.StorageProfileArgs(
+            image_reference=compute.ImageReferenceArgs(
+                offer="WindowsServer",
+                publisher="MicrosoftWindowsServer",
+                sku="2016-Datacenter",
+                version="latest",
+            ),
+            os_disk=compute.OSDiskArgs(
+                caching="ReadWrite",
+                create_option="FromImage",
+                managed_disk=compute.ManagedDiskParametersArgs(
+                    storage_account_type="Standard_LRS",
+                ),
+                name=f"{k}osdisk",
+            ),
+        ),
+        vm_name=k,
+    )
