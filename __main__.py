2d2290b135cfcf68a740a68ba3e92ae2992fd916
juliopdx <juliop503@gmail.com>
2021-08-23 18:17:12 -0700

    adding nat gateway stuffs and VMs with no pip

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@
 import pulumi
 from pulumi import Output
 from pulumi_azure_native import resources, network, compute
-from infra.vms import VM_DATA
+from infra.vms import VM_DATA, VM_DATA_NO_PIP
 from infra.vnets import VNETS
 
 RG_NAME = "juliopdx_rg_dev"
@@ -19,6 +19,35 @@ private_zone = network.PrivateZone(
     location="Global",
     private_zone_name="azure_pulumi.com",
     resource_group_name=resource_group.name,
+)
+
+### NAT Gateway Practice ###
+pip_prefix = network.PublicIPPrefix(
+    "publicIPPrefix",
+    location="westus",
+    prefix_length=31,
+    public_ip_address_version="IPv4",
+    public_ip_prefix_name="test-ipprefix",
+    resource_group_name=resource_group.name,
+    sku=network.PublicIPPrefixSkuArgs(
+        name="Standard",
+        tier="Regional",
+    ),
+)
+
+nat_gateway = network.NatGateway(
+    "natGateway",
+    location="westus",
+    nat_gateway_name="natgateway",
+    public_ip_prefixes=[
+        network.SubResourceArgs(
+            id=pip_prefix.id,
+        )
+    ],
+    resource_group_name=resource_group.name,
+    sku=network.NatGatewaySkuArgs(
+        name="Standard",
+    ),
 )
 
 subs = {}
@@ -48,43 +77,53 @@ for vnet, values in VNETS.items():
     )
     # Create subnets under each VNET
     for subnet in values["subnets"]:
-        sub = network.Subnet(
-            subnet["name"],
-            address_prefix=subnet["subnet"],
-            resource_group_name=resource_group.name,
-            subnet_name=subnet["name"],
-            virtual_network_name=net.name,
-        )
+        if subnet["name"] == "PublicWebServiceSubnet":
+            sub = network.Subnet(
+                subnet["name"],
+                address_prefix=subnet["subnet"],
+                resource_group_name=resource_group.name,
+                subnet_name=subnet["name"],
+                virtual_network_name=net.name,
+                nat_gateway=network.SubResourceArgs(id=nat_gateway.id),
+            )
+        else:
+            sub = network.Subnet(
+                subnet["name"],
+                address_prefix=subnet["subnet"],
+                resource_group_name=resource_group.name,
+                subnet_name=subnet["name"],
+                virtual_network_name=net.name,
+            )
         # Neat trick to grab the subnet IDs and add them to dict
         subs[subnet["name"]] = Output.concat(sub.id)
 
 # VNET peering, fairly static
-vnet_peering = network.VirtualNetworkPeering(
-    "Test-VNET-peer",
-    resource_group_name=resource_group.name,
-    virtual_network_name="CoreServicesVnet",
-    remote_virtual_network=network.SubResourceArgs(id=local_vnets["ResearchVnet"]),
-    virtual_network_peering_name="peer",
-)
+# vnet_peering = network.VirtualNetworkPeering(
+#     "Test-VNET-peer",
+#     resource_group_name=resource_group.name,
+#     virtual_network_name="CoreServicesVnet",
+#     remote_virtual_network=network.SubResourceArgs(id=local_vnets["ResearchVnet"]),
+#     virtual_network_peering_name="peer",
+# )
 
-second_vnet_peering = network.VirtualNetworkPeering(
-    "Test-VNET-peer2",
-    resource_group_name=resource_group.name,
-    virtual_network_name="ResearchVnet",
-    remote_virtual_network=network.SubResourceArgs(id=local_vnets["CoreServicesVnet"]),
-    virtual_network_peering_name="peer",
-)
+# second_vnet_peering = network.VirtualNetworkPeering(
+#     "Test-VNET-peer2",
+#     resource_group_name=resource_group.name,
+#     virtual_network_name="ResearchVnet",
+#     remote_virtual_network=network.SubResourceArgs(id=local_vnets["CoreServicesVnet"]),
+#     virtual_network_peering_name="peer",
+# )
 
 # Create all the things for VM
 for k, v in VM_DATA.items():
     pip = network.PublicIPAddress(
-        f"{k}-publicIPAddress",
+        f"{k}-pip",
         location=v["location"],
         public_ip_address_name=f"{k}-pip",
         resource_group_name=resource_group.name,
     )
     nic = network.NetworkInterface(
-        f"{k}-networkInterface",
+        f"{k}-nic",
         enable_accelerated_networking=True,
         ip_configurations=[
             network.NetworkInterfaceIPConfigurationArgs(
@@ -100,7 +139,63 @@ for k, v in VM_DATA.items():
         resource_group_name=resource_group.name,
     )
     virtual_machine = compute.VirtualMachine(
-        f"{k}-virtualMachine build",
+        f"{k} build",
+        hardware_profile=compute.HardwareProfileArgs(
+            vm_size="Standard_D1_v2",
+        ),
+        location=v["location"],
+        network_profile=compute.NetworkProfileArgs(
+            network_interfaces=[
+                compute.NetworkInterfaceReferenceArgs(
+                    id=nic.id,
+                    primary=True,
+                )
+            ],
+        ),
+        os_profile=compute.OSProfileArgs(
+            admin_password="JulioPDX789!@#",  # Dont do this in prod... or ever, dont be me.
+            admin_username="juliopdx",
+            computer_name=k,
+        ),
+        resource_group_name=resource_group.name,
+        storage_profile=compute.StorageProfileArgs(
+            image_reference=compute.ImageReferenceArgs(
+                offer="WindowsServer",
+                publisher="MicrosoftWindowsServer",
+                sku="2016-Datacenter",
+                version="latest",
+            ),
+            os_disk=compute.OSDiskArgs(
+                caching="ReadWrite",
+                create_option="FromImage",
+                managed_disk=compute.ManagedDiskParametersArgs(
+                    storage_account_type="Standard_LRS",
+                ),
+                name=f"{k}osdisk1",
+            ),
+        ),
+        vm_name=k,
+    )
+
+### Add two VMs with no public IP and test access
+for k, v in VM_DATA_NO_PIP.items():
+    nic = network.NetworkInterface(
+        f"{k}-nic",
+        enable_accelerated_networking=True,
+        ip_configurations=[
+            network.NetworkInterfaceIPConfigurationArgs(
+                name="ipconfig1",
+                subnet=network.SubnetArgs(
+                    id=subs[v["nic_subnet"]],
+                ),
+            )
+        ],
+        location=v["location"],
+        network_interface_name=v["nic_name"],
+        resource_group_name=resource_group.name,
+    )
+    virtual_machine = compute.VirtualMachine(
+        f"{k} build",
         hardware_profile=compute.HardwareProfileArgs(
             vm_size="Standard_D1_v2",
         ),
@@ -139,31 +234,43 @@ for k, v in VM_DATA.items():
     )
 
 
-### NAT Gateeway Practice ###
-pip_prefix = network.PublicIPPrefix(
-    "publicIPPrefix",
-    location="westus",
-    prefix_length=31,
-    public_ip_address_version="IPv4",
-    public_ip_prefix_name="test-ipprefix",
-    resource_group_name=resource_group.name,
-    sku=network.PublicIPPrefixSkuArgs(
-        name="Standard",
-        tier="Regional",
-    ),
-)
+# Virtual WAN Stuffs and VNET Gateways
+# No more, it takes so long to build and destroy
+# virtual_wan = network.VirtualWan(
+#     "TestVirtualWan",
+#     allow_branch_to_branch_traffic=True,
+#     allow_vnet_to_vnet_traffic=True,
+#     resource_group_name=resource_group.name,
+#     location=resource_group.location,
+#     type="Standard",
+#     virtual_wan_name="wan1",
+# )
 
-nat_gateway = network.NatGateway(
-    "natGateway",
-    location="westus",
-    nat_gateway_name="natgateway",
-    public_ip_prefixes=[
-        network.SubResourceArgs(
-            id=pip_prefix.id,
-        )
-    ],
-    resource_group_name=resource_group.name,
-    sku=network.NatGatewaySkuArgs(
-        name="Standard",
-    ),
-)
+# virtual_hub = network.VirtualHub(
+#     "TestVirtualHub",
+#     resource_group_name=resource_group.name,
+#     location=resource_group.location,
+#     virtual_wan=network.SubResourceArgs(id=virtual_wan.id),
+#     sku="Standard",
+#     virtual_hub_name="virtualHug0",
+#     address_prefix="10.60.0.0/24",
+# )
+
+# vpn_gateway = network.VpnGateway(
+#     "VPN Gateway",
+#     gateway_name="myGate",
+#     resource_group_name=resource_group.name,
+#     virtual_hub=network.SubResourceArgs(id=virtual_hub.id),
+#     vpn_gateway_scale_unit=1,
+# )
+
+# hub_virtual_network_connection = network.HubVirtualNetworkConnection(
+#     "hubVirtualNetworkConnection",
+#     connection_name="connection1",
+#     enable_internet_security=False,
+#     remote_virtual_network=network.SubResourceArgs(
+#         id=local_vnets["ResearchVnet"],
+#     ),
+#     resource_group_name=resource_group.name,
+#     virtual_hub_name=virtual_hub.name,
+# )
--- infra/vms.py
+++ infra/vms.py
@@ -1,23 +1,35 @@
 VM_DATA = {
     "testvm1": {
-        "rg_name": "juliopdx_rg_dev",
         "nic_name": "testvm1-nic",
         "nic_vnet": "CoreServicesVnet",
         "nic_subnet": "DatabaseSubnet",
         "location": "westus",
     },
     "testvm2": {
-        "rg_name": "juliopdx_rg_dev",
         "nic_name": "testvm2-nic",
         "nic_vnet": "CoreServicesVnet",
         "nic_subnet": "DatabaseSubnet",
         "location": "westus",
     },
     "testvm3": {
-        "rg_name": "juliopdx_rg_dev",
         "nic_name": "testvm3-nic",
         "nic_vnet": "ResearchVnet",
         "nic_subnet": "ResearchSystemSubnet",
         "location": "westindia",
+    },
+}
+
+VM_DATA_NO_PIP = {
+    "testvm4": {
+        "nic_name": "testvm4-nic",
+        "nic_vnet": "CoreServicesVnet",
+        "nic_subnet": "PublicWebServiceSubnet",
+        "location": "westus",
+    },
+    "testvm5": {
+        "nic_name": "testvm5-nic",
+        "nic_vnet": "CoreServicesVnet",
+        "nic_subnet": "PublicWebServiceSubnet",
+        "location": "westus",
     },
 }
